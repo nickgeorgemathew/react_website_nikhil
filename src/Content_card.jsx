856684c8b7445821a71f394d3b8ce7ac58c342c4
nickgeorgemathew <148985027+nickgeorgemathew@users.noreply.github.com>
2024-07-17 12:55:47 +0530

    image background for cards and height and overflow

--- src/Content_card.jsx
+++ src/Content_card.jsx
@@ -15,7 +15,7 @@ export  const Content_card =() => {
     //container
     <div className="flex flex-col items-center bg-amber-700 w-screen h-screen p-0 relative text-center  mt-0 ">
       {/* First card */}
-      <div className="  text-slate-50 flex flex-col  items-center justify-center p-0 w-screen left-0 right-0 bg-slate-950 min-h-screen ">
+      <div className="  text-slate-50 flex flex-col  items-center justify-center p-0 w-screen left-0 right-0 bg-slate-950 min-h-screen bg-[url('..\src\assets\OIP.jpg')] bg-cover bg-no-repeat">
         {/*  card content */}
         
       <p>SDFSDFWEFW</p>
@@ -33,7 +33,7 @@ export  const Content_card =() => {
       </div>
 
       {/* Second card */}
-      <div className=" flex flex-col p-0 w-screen left-0 right-0 bg-slate-300 min-h-screen ">
+      <div className=" flex flex-col items-center justify-center p-0 w-screen left-0 right-0 bg-slate-300 min-h-screen ">
         {/* card content */}
         <p>sdfsdfsdfqwqeqweqweqwe</p>
       {/*button */}
