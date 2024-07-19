58f491d856c2cf41237b2c51050db4c6becd2975
nickgeorgemathew <148985027+nickgeorgemathew@users.noreply.github.com>
2024-07-19 19:50:13 +0530

    .

--- src/Content_card.jsx
+++ src/Content_card.jsx
@@ -1,25 +1,29 @@
-import image1 from './OIP.jpg'
-import image2 from './qq.jpg'
+import image1 from './w.jpg'
+
 import image3 from './ss.jpg'
+import image4 from './wp2.jpg'
 
 
 const bgarray=[
   {
     num:1,
     title:"one",
-    img:image1
+    img:image1,
+    datas:"sdasdasdsadasda"
   }
   ,
-  {
-    num:2,
-    title:"two",
-    img:image2
-  }
-,
+  
 {
   num:3,
   title:"two",
-  img:image3
+  img:image3,
+  datas:"sdasdasdsadasda"
+},
+{
+  num:4,
+  title:"two",
+  img:image4,
+  datas:"sdasdasdsadasda"
 }
 
 
@@ -28,17 +32,22 @@ const bgarray=[
 
 ]
 const res=bgarray.map((item,index)=>(
-  <div key={index} className="  text-slate-50 flex flex-col  items-center justify-center p-0 w-screen left-0 right-0 bg-slate-950 min-h-screen  bg-cover bg-no-repeat"style={{ backgroundImage: `url(${item.img})` }}>
+  <div key={index} className="  text-slate-50 flex flex-col   items-center justify-center p-0 w-screen left-0 right-0 bg-slate-950 min-h-screen  bg-cover bg-no-repeat"style={{ backgroundImage: `url(${item.img})` }}>
         {/*  card content */}
-        
-      <p>{item.title}</p>
-          {/*button */}
         <div>
-          <button>
+          <p className='block '>{item.title}</p>
+          <p>{item.datas}</p>
+        </div>
+        
+      
+          {/*button */}
+        <div className=' flex flex-row gap-28 w-full h-2/4 pt-96 items-center justify-center'>
+          <button className=' opacity-100 bg-opacity-0 mt-50 bg-transparent hover:bg-black text-black font-semibold hover:text-white py-2 px-4 border-4 border-black border-solid hover:border-transparent rounded h-10
+          w-1/6'>
             ordernow
 
           </button>
-          <button>
+          <button className=' w-1/6 mt-50 bg-transparent hover:bg-white text-white font-semibold hover:text-black py-2 px-4 border border-white-500 hover:border-transparent rounded h-10'>
             demo drive
             
           </button>
@@ -50,9 +59,10 @@ const res=bgarray.map((item,index)=>(
 export  const Content_card =() => {
   return (
     //container
-    <div className="flex flex-col items-center bg-amber-700 w-screen h-screen p-0 relative text-center  mt-0 ">
-      {/* First card */}
+    <div className="flex  flex-col items-center bg-amber-700 w-screen h-screen p-0 relative text-center  mt-0 ">
+      {/*  card */}
       {res}
+      {/* card */}
       
 
      
