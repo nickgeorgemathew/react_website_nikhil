9865d5c9f8424a87e03c3d8cb40a01535ede53f7
nickgeorgemathew <148985027+nickgeorgemathew@users.noreply.github.com>
2024-08-08 20:33:01 +0530

    button size fit for all screen sizes

--- src/Content_card.jsx
+++ src/Content_card.jsx
@@ -43,13 +43,13 @@ const res=bgarray.map((item,index)=>(
       
           {/*button */}
         <div className=' flex flex-row gap-28 w-full h-2/4 pt-96 items-center justify-center'>
-          <button className=' opacity-100 bg-opacity-0 mt-50 bg-transparent hover:bg-black text-black font-semibold hover:text-white py-2 px-4 border-4 border-black border-solid hover:border-transparent rounded h-10
+          <button className=' mob_s:w-fit sm:w-1/6 opacity-100 bg-opacity-0 mt-50 bg-transparent hover:bg-black text-black font-semibold hover:text-white py-2 px-4 border-4 border-black border-solid hover:border-transparent rounded h-10
           w-1/6'>
             ordernow
 
           </button>
-          <button className=' w-1/6 mt-50 bg-transparent hover:bg-white text-white font-semibold hover:text-black py-2 px-4 border border-white-500 hover:border-transparent rounded h-10'>
-            demo drive
+          <button className=' mob_s:w-fit sm:w-1/6 mt-50 bg-transparent hover:bg-white text-white font-semibold hover:text-black py-2 px-4 border border-white-500 hover:border-transparent rounded h-10'>
+            demodrive
             
           </button>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,29 @@ export default {
         'height': 'height',
       },
     },
+    
+      screens: {
+        'mob_s':'320px',
+        // => @media (min-width: 320px) { ... }
+        'mob_m':'375px',
+        // => @media (min-width:375px) { ... }
+        'mob_l':'425px',
+        // => @media (min-width: 425px) { ... }
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      }
+    
+  
   },
+  
   plugins: [],
 }
